20524d4b8479ed4f183ee39c54dbe2cecb23ded4
Khandaker Ziaur Rahman Rehan <zrrehan@outlook.com>
2025-09-21 20:12:36 +0600

    Added basic banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,18 +1,38 @@
+import { TypeAnimation } from "react-type-animation";
 import myImg from "../assets/DP.png"
 
 function Banner() {
     return(
-        <div>
+        <div className="flex py-[150px] w-[90%] max-w-[1250px] mx-auto items-center gap-20">
             <div className="md:w-[50%]">
                 <div className="w-[500px]">
                     <img src={myImg} className="w-full rounded-full" />
                 </div>
-
-                <div>
-                    <h1>Hello, I am Rehan</h1>
-                    
-                </div>
             </div>
+            <div className="divider divider-horizontal"></div>
+            <div className="">
+                <h1 className=" text-5xl font-bold">Hello, I am  <span className=" text-6xl monoton-regular">Rehan</span></h1>
+                <TypeAnimation
+                    sequence={[
+                        // Same substring at the start will only be typed once, initially
+                        'A Frontend Developer',
+                        1000,
+                        'A React Enthusiast',
+                        1000,
+                        'A Problem Solver',
+                        1000,
+                        'A Web Developer',
+                        1000,
+                    ]}
+                    speed={50}
+                    style={{ fontSize: '35px', fontWeight: "bold" }}
+                    repeat={Infinity}
+                ></TypeAnimation>
+                <p>
+                    Passionate frontend developer skilled in React, creating responsive, user-friendly, and high-performance web applications with a focus on clean design and seamless user experience.
+                </p>
+            </div>
+            
         </div>
     )
 }
