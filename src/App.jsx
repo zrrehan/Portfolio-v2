905cc952c1f095ac8150ca605781634672211423
Khandaker Ziaur Rahman Rehan <zrrehan@outlook.com>
2025-09-21 23:29:30 +0600

    added react-animated-cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Banner from "./Components/Banner/Banner"
 import Navbar from "./Components/Navbar"
 import Skills from "./Components/Skills/Skills"
+import AnimatedCursor from "react-animated-cursor"
 
 function App() {
     return (
         <>
+            <AnimatedCursor innerStyle={{ backgroundColor: 'rgb(0, 0, 0)' }}
+                outerStyle={{ backgroundColor: 'rgb(194, 194, 194)', border: '2px solid rgb(0, 0, 0)' }} />
             <Navbar></Navbar>
             <Banner></Banner>
             <Skills></Skills>
